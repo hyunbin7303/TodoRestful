f9a8934ff1842f47a1e6c62f6b1353e3854f84d1
bds0900 <Dbeak9336@conestogac.on.ca>
2020-07-08 12:28:44 -0400

    add Item model

---
+++ TodoApi.Model/Item/Items.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoApi.Model.Item
+{
+    class Items
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime? PurchaseDate { get; set; }
+        public ItemType ItemType { get; set; }
+    }
+    public enum ItemType
+    {
+        Food,
+        Car,
+        House,
+        Entertainment,
+        Electronic,
+        Others
+    }
+}
