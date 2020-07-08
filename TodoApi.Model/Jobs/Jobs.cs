7a92ada49da9c6dd1bd4b3ec6222fe24599d0c5a
bds0900 <Dbeak9336@conestogac.on.ca>
2020-07-08 11:11:58 -0400

    add Job model

---
+++ TodoApi.Model/Jobs/Jobs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoApi.Model.Jobs
+{
+    class Jobs
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Goal { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public DateTime? ExpectedEndTime { get; set; }
+        public JobType JobType { get; set; }
+    }
+}
