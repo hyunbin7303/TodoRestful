7eb39e86c77a1bba7a83accd335a6d363bfde4c6
bds0900 <Dbeak9336@conestogac.on.ca>
2020-07-25 22:29:25 -0400

    Add Search method. This method is similar to find method. It searches a keywrod from text while find only find only can math a word.

--- TodoApi.Datasource/MongoRepository.cs
+++ TodoApi.Datasource/MongoRepository.cs
@@ -17,6 +17,10 @@ namespace TodoApi.Datasource
         {
             var db = new MongoClient(settings.Connection).GetDatabase(settings.DatabaseName);
             _collection = db.GetCollection<TDocument>(GetCollectionName(typeof(TDocument)));
+
+            // indexing specified field, in here index UserId field
+            //var indexModel = new CreateIndexModel<TDocument>(Builders<TDocument>.IndexKeys.Text(model => model.UserId));
+            //_collection.Indexes.CreateOne(indexModel);
         }
         private protected string GetCollectionName(Type documentType)
         {
@@ -148,6 +152,11 @@ namespace TodoApi.Datasource
         public Task<IList<TDocument>> FindAll()
         {
             return Task.FromResult<IList<TDocument>>(_collection.Find(Builders<TDocument>.Filter.Empty).ToList());
+        }
+        public Task<IList<TDocument>> Search(string keyword)
+        {
+            var filter = Builders<TDocument>.Filter.Text(keyword);
+            return Task.FromResult<IList<TDocument>>(_collection.Find(filter).ToList());
         }
 
     }
