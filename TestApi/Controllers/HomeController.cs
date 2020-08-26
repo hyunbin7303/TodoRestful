1a7dd5f158525a3d2660d59e3000ac57f085e5a9
bds0900 <Dbeak9336@conestogac.on.ca>
2020-08-26 08:57:48 -0400

    Add TestApi

---
+++ TestApi/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApi.Controllers
+{
+    public class HomeController:Controller
+    {
+
+        [Authorize]
+        public string Secret()
+        {
+            return "This is secret message";
+        }
+    }
+}
