f424e9e619e9eafc3427ec643a427d5d7390991a
bds0900 <Dbeak9336@conestogac.on.ca>
2020-09-28 21:30:42 -0400

    Update ConverTo

--- TodoService/TodoApi.Infrastructure/Extensions/Extension.cs
+++ TodoService/TodoApi.Infrastructure/Extensions/Extension.cs
@@ -25,15 +25,6 @@ namespace TodoApi.Infrastructure.Extensions
 
             return source.Select(todo =>iMapper.Map<Todo, TodoDTO>(todo));
         }
-        public static IEnumerable<TDestination> DynConvertTo<TSource,TDestination>(this IEnumerable<TSource> source)
-        {
-            var config = new MapperConfiguration(cfg => {
-                cfg.CreateMap<TSource, TDestination>();
-            });
-            IMapper iMapper = config.CreateMapper();
-
-            return source.Select(todo => iMapper.Map<TSource, TDestination>(todo));
-        }
         public static TodoDTO ConvertTo(this Todo source)
         {
             var config = new MapperConfiguration(cfg => {
@@ -41,6 +32,24 @@ namespace TodoApi.Infrastructure.Extensions
             });
             IMapper iMapper = config.CreateMapper();
             return iMapper.Map<Todo, TodoDTO>(source);
+        }
+        public static IEnumerable<TDestination> ConvertTo<TSource,TDestination>(this IEnumerable<TSource> source)
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<TSource, TDestination>().ReverseMap();
+            });
+            IMapper iMapper = config.CreateMapper();
+
+            return source.Select(todo => iMapper.Map<TSource, TDestination>(todo));
+        }
+        public static TDestination ConvertTo<TSource, TDestination>(this TSource source)
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<TSource, TDestination>().ReverseMap();
+            });
+            IMapper iMapper = config.CreateMapper();
+
+            return iMapper.Map<TSource, TDestination>(source);
         }
 
     }
